d93e34d20c370d40c41410f5e9f6032394e83c06
Geenons david <geenonsdavid@gmail.com>
2024-01-18 19:18:45 +0100

    add open times

--- lib/components/timesopen.dart
+++ lib/components/timesopen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garage_parrot/themes/colors.dart';
 
 class TimesOpen extends StatefulWidget {
   const TimesOpen
@@ -11,20 +12,113 @@ class TimesOpen extends StatefulWidget {
 class _TimesOpenState extends State<TimesOpen> {
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Row(
-            children: [
-              Text("horaire d'ouverture"),
-            ],
-          ),
+    String mondayAm = "9h - 12h";
+    String mondayPm = "fermé";
+    String tuesdayAm = mondayAm;
+    String tuesdayPm = "14h - 19h";
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          width: 1,
+          color: secondary,
         ),
-      ],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: secondary,
+                width: 1)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text("horaire d'ouverture",
+                style: Theme.of(context).textTheme.headlineMedium,),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Column(
+      
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Lundi : '),
+                        Text('$mondayAm / $mondayPm'),
+                            
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Mardi : '),
+                        Text('$tuesdayAm / $tuesdayPm'),
+                      ],
+                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Mercredi : '),
+                        Text('$tuesdayAm / $tuesdayPm'),
+                      ],
+                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Jeudi : '),
+                        Text('$tuesdayAm / $tuesdayPm'),
+                      ],
+                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Vendredi : '),
+                        Text('$tuesdayAm / $tuesdayPm'),
+                            
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Samedi : '),
+                        Text('$tuesdayAm / $tuesdayPm'),
+                            
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Text('Dimanche : '),
+                        Text('$mondayPm / $mondayPm'),
+                            
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
