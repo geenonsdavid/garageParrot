d74193bb6098789bc19c5c8749e3610c38de4369
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2024-01-22 18:59:35 +0100

    add provider

--- lib/components/radio_gearbox.dart
+++ lib/components/radio_gearbox.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:garage_parrot/providers/caracteristic_provider.dart';
+import 'package:provider/provider.dart';
 
 class RadioGearbox extends StatefulWidget {
   const RadioGearbox({super.key});
@@ -6,38 +8,58 @@ class RadioGearbox extends StatefulWidget {
   @override
   State<RadioGearbox> createState() => _RadioGearboxState();
 }
+
 class _RadioGearboxState extends State<RadioGearbox> {
   String? selectedGearbox;
+  late CaracteristicProvider caracteristicProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    caracteristicProvider =
+        Provider.of<CaracteristicProvider>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Row(
-      children: [
-        Text('Type de boite :',
-        style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-          color: Colors.black
+          children: [
+            Text(
+              'Type de boite :',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall
+                  ?.copyWith(color: Colors.black),
+            ),
+          ],
         ),
-        ),
-      ],
-    ),
         Row(
           children: [
-            Radio(value: "automatic", groupValue: selectedGearbox, onChanged: (String? value) {
-                    setState(() {
-                      selectedGearbox = value;
-                    });
-                  }),
+            Radio(
+                value: "automatic",
+                groupValue: selectedGearbox,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedGearbox = value;
+                    caracteristicProvider.updateSelectedGearbox(selectedGearbox);
+                  });
+                }),
             const Text('Automatique'),
           ],
         ),
         Row(
           children: [
-            Radio(value: "manual", groupValue: selectedGearbox, onChanged: (String? value) {
-                    setState(() {
-                      selectedGearbox = value;
-                    });
-                  }),
+            Radio(
+                value: "manual",
+                groupValue: selectedGearbox,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedGearbox = value;
+                    caracteristicProvider.updateSelectedGearbox(selectedGearbox);
+                  });
+                }),
             const Text('Manuelle'),
           ],
         ),
