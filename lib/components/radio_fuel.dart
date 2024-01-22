6b194f75398c6dc9b7160d6f6f5c3e786030a64d
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2024-01-22 18:31:59 +0100

    add provider

--- lib/components/radio_fuel.dart
+++ lib/components/radio_fuel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:garage_parrot/providers/caracteristic_provider.dart';
+import 'package:provider/provider.dart';
 
 class RadioFuel extends StatefulWidget {
   const RadioFuel({super.key});
@@ -9,10 +11,18 @@ class RadioFuel extends StatefulWidget {
 
 class _RadioFuelState extends State<RadioFuel> {
   String? selectedFuel;
+  late CaracteristicProvider caracteristicProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    caracteristicProvider =
+        Provider.of<CaracteristicProvider>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
+    return Column(children: [
       Row(
         children: [
           Text(
@@ -25,65 +35,89 @@ class _RadioFuelState extends State<RadioFuel> {
         ],
       ),
       Row(
-          children: [
-            Radio(value: "Diesel", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('Diesel'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "Diesel",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('Diesel'),
+        ],
+      ),
       Row(
-          children: [
-            Radio(value: "essence", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('Essence'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "essence",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('Essence'),
+        ],
+      ),
       Row(
-          children: [
-            Radio(value: "hybribe", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('hybride'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "hybribe",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('hybride'),
+        ],
+      ),
       Row(
-          children: [
-            Radio(value: "electrique", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('Electrique'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "electrique",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('Electrique'),
+        ],
+      ),
       Row(
-          children: [
-            Radio(value: "gpl", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('GPL'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "gpl",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('GPL'),
+        ],
+      ),
       Row(
-          children: [
-            Radio(value: "autre", groupValue: selectedFuel, onChanged: (String? value) {
-                    setState(() {
-                      selectedFuel = value;
-                    });
-                  }),
-            const Text('Autre'),
-          ],
-        ),
+        children: [
+          Radio(
+              value: "autre",
+              groupValue: selectedFuel,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedFuel = value;
+                  caracteristicProvider.updateSelectedFuel(selectedFuel);
+                });
+              }),
+          const Text('Autre'),
+        ],
+      ),
     ]);
   }
 }
