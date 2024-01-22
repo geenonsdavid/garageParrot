1f0b4cbf0b7d72cb3ec1f6cbc1fbe242c2bdd929
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2024-01-22 18:31:35 +0100

    add provider

--- lib/components/radio_door.dart
+++ lib/components/radio_door.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:garage_parrot/providers/caracteristic_provider.dart';
+import 'package:provider/provider.dart';
 
 class RadioDoor extends StatefulWidget {
-  const RadioDoor({super.key});
-
+  const RadioDoor({Key? key}) : super(key: key);
   @override
   State<RadioDoor> createState() => _RadioDoorState();
 }
 
 class _RadioDoorState extends State<RadioDoor> {
+  late CaracteristicProvider caracteristicProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    caracteristicProvider = Provider.of<CaracteristicProvider>(context, listen: false);
+  }
+
   int? selectedDoor;
   @override
   Widget build(BuildContext context) {
@@ -17,59 +24,32 @@ class _RadioDoorState extends State<RadioDoor> {
       children: [
         Row(
           children: [
-            Text('Nombre de portes :',
-            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-              color: Colors.black
+            Text(
+              'Nombre de portes :',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: Colors.black,
+                  ),
             ),
-            ),
           ],
         ),
-        Row(
+        Column(
           children: [
-            Radio(value: 2,
-            groupValue: selectedDoor,
-            onChanged: (int? value) {
-                    setState(() {
-                      selectedDoor = value;
-                    });
-                  }),
-            const Text('2'),
-          ],
-        ),
-        Row(
-          children: [
-            Radio(value: 3,
-            groupValue: selectedDoor,
-            onChanged: (int? value) {
-                    setState(() {
-                      selectedDoor = value;
-                    });
-                  }),
-            const Text('3'),
-          ],
-        ),
-        Row(
-          children: [
-            Radio(value: 4,
-            groupValue: selectedDoor,
-            onChanged: (int? value) {
-                    setState(() {
-                      selectedDoor = value;
-                    });
-                  }),
-            const Text('4'),
-          ],
-        ),
-        Row(
-          children: [
-            Radio(value: 5,
-            groupValue: selectedDoor,
-            onChanged: (int? value) {
-                    setState(() {
-                      selectedDoor = value;
-                    });
-                  }),
-            const Text('5'),
+            for (int i = 2; i <= 5; i++)
+              Row(
+                children: [
+                  Radio(
+                    value: i,
+                    groupValue: selectedDoor,
+                    onChanged: (int? value) {
+                      setState(() {
+                        selectedDoor = value;
+                        caracteristicProvider.updateSelectedDoor(selectedDoor);
+                      });
+                    },
+                  ),
+                  Text('$i'),
+                ],
+              ),
           ],
         ),
       ],
