59c3f2b071b54ed425e59e9b554f26aa34ed3c09
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2024-01-25 09:29:22 +0100

    create list option

---
+++ lib/list_options.dart
@@ -0,0 +1,6 @@
+final List<Map<String, dynamic>> switchOptionsData = [
+    {"id": 1, "name": "Vitre électrique avant", "option": "Option 1", "value": false},
+    {"id": 2, "name": "Vitre électrique arrière", "option": "Option 2", "value": false},
+    {"id": 3, "name": "Climatisation", "option": "Option 3", "value": false},
+    // Ajoutez autant d'options que nécessaire
+  ];
