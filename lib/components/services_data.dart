62b421939949b815dbd1231fb44dbba2065e9d0e
Geenons david <geenonsdavid@gmail.com>
2024-02-06 09:05:10 +0100

    data services

---
+++ lib/components/services_data.dart
@@ -0,0 +1,58 @@
+const List<Map<String, String>> servicesData = [
+  {
+    "image" : "assets/services/service_clim.jpg",
+    "title": "Climatisation",
+    "description" :"réparation climatisation",
+  },
+  {
+    "image" : "assets/services/service_diagnostic.jpg",
+    "title":"Diagnostique",
+    "description": "Diagnostique",
+  },
+  {
+    "image" : "assets/services/service_distribution.jpg",
+    "title":"Distribution",
+    "description": "réparation distribution",
+  },
+  {
+    "image" : "assets/services/service_embrayage.jpg",
+    "title":"Embrayage",
+    "description": "réparation embrayage",
+  },
+  {
+    "image" : "assets/services/service_frein.jpg",
+    "title":"Frein",
+    "description": "réparation frein",
+  },
+  {
+    "image" : "assets/services/service_meca.jpg",
+    "title":"Mécanique",
+    "description": "réparation",
+  },
+  {
+    "image" : "assets/services/service_pneu.jpg",
+    "title":"Pneu",
+    "description": "réparation pneu",
+  },
+  {
+    "image" : "assets/services/service_vidange.jpg",
+    "title":"Vidange",
+    "description": "Vidange périodique",
+  },
+  {
+    "image" : "assets/services/service_wheel.jpg",
+    "title":"Wheel",
+    "description": "Vidange périodique",
+  },
+  {
+    "image" : "assets/services/service-suspension.jpg",
+    "title":"Suspension",
+    "description": "Suspension",
+  },
+  {
+    "image" : "assets/services/service1.jpg",
+    "title":"Carrosserie",
+    "description": "Carrosserie",
+  },
+  
+];
