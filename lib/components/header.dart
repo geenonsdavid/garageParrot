86965cb19345d30b872755c096e51d0de094114b
Geenons david <geenonsdavid@gmail.com>
2024-02-06 13:05:40 +0100

    add navigator

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,110 +1,111 @@
-import "package:flutter/material.dart";
-import "package:flutter_svg/svg.dart";
-import "package:garage_parrot/components/build_nav_button.dart";
-import "package:garage_parrot/themes/colors.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:garage_parrot/components/build_nav_button.dart';
+import 'package:garage_parrot/themes/colors.dart';
+import 'package:garage_parrot/views/admin_view.dart';
+import 'package:garage_parrot/views/homepage.dart';
+import 'package:garage_parrot/views/shop_view.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
   const Header({
-    super.key,
+    Key? key,
     required this.title,
     required this.isScreenPhone,
     this.onLogoutPressed,
-  });
+  }) : super(key: key);
 
   final String title;
   final bool isScreenPhone;
   final VoidCallback? onLogoutPressed;
 
   @override
-  // height of header
+  // Taille préférée de l'en-tête
   Size get preferredSize => const Size.fromHeight(152);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: primary,
-      child: Column(
+    return AppBar(
+      backgroundColor: primary,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          //SizeBox
-          SizedBox(
-            height: isScreenPhone ? 20 : 8,
-            width: double.infinity,
+          Image.asset(
+            'assets/logo_garage_parrot.png',
+            height: isScreenPhone ? 50 : 100,
+            width: isScreenPhone ? 50 : 100,
           ),
-          // title section
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 8.0, top: 0, right: 8, bottom: 4),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/logo_garage_parrot.png',
-                  height: isScreenPhone ? 50 : 100,
-                  width: isScreenPhone ? 50 : 100,
-                ),
-                Text(title,
-                    style: Theme.of(context).textTheme.headlineLarge),
-                Row(
-                  children: [
-                    // button connexion
-                    IconButton(
-                      onPressed: () {
-                        //gérer l'évévement du bouton
-                      },
-                      icon: SvgPicture.asset(
-                        'assets/icons/power-off-solid.svg',
-                        colorFilter: const ColorFilter.mode(
-                          secondary,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    if (!isScreenPhone)
-                      Text(
-                        'Connexion',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                  ],
-                ),
-              ],
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          if (!isScreenPhone)
+            Text(
+              'Connexion',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-          // nav buttons
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 8.0, top: 4, right: 8, bottom: 4),
-            child: DecoratedBox(
-              decoration: const BoxDecoration(
-                color: primary,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Accueil
-                  buildNavButton(
-                    'assets/icons/house-solid.svg',
-                    'Accueil',
-                    isScreenPhone,
-                    context,
-                  ),
-                  // Galerie
-                  buildNavButton(
-                    'assets/icons/car-on-solid.svg',
-                    'Galerie',
-                    isScreenPhone,
-                    context,
-                  ),
-                  buildNavButton(
-                    'assets/icons/admin.svg',
-                    'Administrateur',
-                    isScreenPhone,
-                    context,
-                  ),
-                ],
+          if (onLogoutPressed != null)
+            IconButton(
+              onPressed: onLogoutPressed,
+              icon: SvgPicture.asset(
+                'assets/icons/power-off-solid.svg',
+                colorFilter: const ColorFilter.mode(
+                  secondary,
+                  BlendMode.srcIn,
+                ),
               ),
             ),
-          ),
         ],
+      ),
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(48),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildNavButton(
+                'assets/icons/house-solid.svg',
+                'Accueil',
+                isScreenPhone,
+                context,
+                () {
+                  // Naviguer vers la page d'accueil
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage(title: title))
+                  );
+                },
+              ),
+              buildNavButton(
+                'assets/icons/car-on-solid.svg',
+                'Galerie',
+                isScreenPhone,
+                context,
+                () {
+                  // Naviguer vers la page de la galerie
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ShopView(title: title))
+                  );
+                },
+              ),
+              buildNavButton(
+                'assets/icons/admin.svg',
+                'Administrateur',
+                isScreenPhone,
+                context,
+                () {
+                  // Naviguer vers la page d'administrateur
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdminView(title: title))
+                  );
+                  
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
