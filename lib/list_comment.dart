ec1b0976c8a697adcc9c985e94f068706304ce1d
Geenons david <geenonsdavid@gmail.com>
2024-02-07 16:11:21 +0100

    list comment

---
+++ lib/list_comment.dart
@@ -0,0 +1,12 @@
+List comments = [
+  {
+    "pseudoMessage": "toto",
+    "message": "Ceci est le premier message",
+    "valid": false
+  },
+  {
+    "pseudoMessage": "Claude",
+    "message": "Ceci est le deuxième message",
+    "valid": false
+  },
+];
