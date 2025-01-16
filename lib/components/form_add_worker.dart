2eeffee3676749887758c2b6516c4ad15a7fefdb
Geenons david <geenonsdavid@gmail.com>
2025-01-16 16:34:01 +0100

    add method insertrecord and delete method submit

--- lib/components/form_add_worker.dart
+++ lib/components/form_add_worker.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:garage_parrot/components/customfield.dart';
 import 'package:garage_parrot/themes/colors.dart';
+import 'package:http/http.dart' as http;
 
 class FormAddWorker extends StatefulWidget {
   const FormAddWorker({super.key});
@@ -20,7 +23,48 @@ class _FormAddWorkerState extends State<FormAddWorker> {
   final _lastnameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
-  final _messageController = TextEditingController();
+
+  Future<void> insertrecord() async {
+    if (_nameController.text.isNotEmpty &&
+        _lastnameController.text.isNotEmpty &&
+        _emailController.text.isNotEmpty &&
+        _phoneController.text.isNotEmpty) {
+      try {
+        String uri = "http://192.168.1.69/garageparrot_api/insert_record.php";
+
+        var  res = await http.post(Uri.parse(uri), body: {
+          "name": _nameController.text,
+          "lastname": _lastnameController.text,
+          "email": _emailController.text,
+          "phone": _phoneController.text
+        });
+        print("Réponse du serveur : ${res.body}");
+
+        if (res.statusCode != 200) {
+          print("Erreur lors de la connexion au serveur");
+          return;
+        }
+        try {
+          var response = jsonDecode(res.body);
+          if (response["success"] == "true") {
+            print("Employé créé avec succès");
+          } else {
+            print("Erreur lors de la création de l'employé");
+          }
+        } catch (e) {
+          print("erreur de décodage JSON: $e");
+        } 
+        } catch (e) {
+        print("erreur de requête: $e");
+      } 
+      } else {
+        print("Veuillez remplir tous les champs");
+      }
+    }
+  
+
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +72,11 @@ class _FormAddWorkerState extends State<FormAddWorker> {
       margin: const EdgeInsets.all(8),
       padding: const EdgeInsets.all(4),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: secondary,
-          width: 1,
-        ),
-        borderRadius: const BorderRadius.all(Radius.circular(8))
-      ),
+          border: Border.all(
+            color: secondary,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(8))),
       child: Form(
         key: _formKey,
         child: Column(
@@ -46,7 +89,6 @@ class _FormAddWorkerState extends State<FormAddWorker> {
               customHintText: "Veuillez le nom de l'employé",
               customFocus: _nameFocusNode,
               customRequestFocus: _lastnameFocusNode,
-              
             ),
             const SizedBox(height: 10),
             CustomField(
@@ -68,7 +110,7 @@ class _FormAddWorkerState extends State<FormAddWorker> {
             ),
             const SizedBox(height: 10),
             CustomField(
-              controller : _phoneController,
+              controller: _phoneController,
               context: context,
               customLabel: "Téléphone",
               customHintText: "Veuillez entrer votre téléphone",
@@ -76,22 +118,12 @@ class _FormAddWorkerState extends State<FormAddWorker> {
               customRequestFocus: _messageFocusNode,
             ),
             const SizedBox(height: 10),
-            CustomField(
-              controller : _messageController,
-              context: context,
-              customLabel: "Message",
-              customHintText: "Veuillez entrer votre message",
-              customFocus: _messageFocusNode,
-              customRequestFocus: _messageFocusNode,
-              maxLines: 4,
-            ),
-            const SizedBox(height: 10),
+
             ElevatedButton.icon(
               icon: const Icon(Icons.task_alt),
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
-                  // send form at administrator email
-                  submit();
+                  insertrecord();
                 }
               },
               label: const Text("Envoyez"),
@@ -102,14 +134,4 @@ class _FormAddWorkerState extends State<FormAddWorker> {
     );
   }
 
-  void submit() {
-    // Affiche les valeurs saisies
-    debugPrint('Nom: ${_nameController.text}');
-    debugPrint('Prénom: ${_lastnameController.text}');
-    debugPrint('Email: ${_emailController.text}');
-    debugPrint('Téléphone : ${_phoneController.text}');
-    debugPrint('Message: ${_messageController.text}');
-    
-
-  }
 }
