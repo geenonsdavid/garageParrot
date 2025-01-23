170173024ebd4a86b63a2707550c8d010abea55b
Geenons david <geenonsdavid@gmail.com>
2025-01-23 14:16:10 +0100

    change in stateful

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:garage_parrot/themes/colors.dart';
+import 'package:garage_parrot/components/formlogin.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(color: secondary),
-        title: Text('Login',
-        style: Theme.of(context).textTheme.headlineLarge,),
+        title: const Text('Login'),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: background,
-          border: Border.all(color: secondary, width: 2),
-          borderRadius: BorderRadius.circular(10),
-          ),
-        
-        child: const Column(
-          children: [
-            // field for email
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: secondary),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: secondary),
-                ),
-              ),
-            ),
-            // field for password
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: secondary),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: secondary),
-                ),
-              ),
-            ),
-            Center(
-              child: Text('Login', style: TextStyle(color: Colors.black)),
-            ),
-          ],
-        ),
-      ),
-    );
+      body: const FormLogin(), );
   }
 }
