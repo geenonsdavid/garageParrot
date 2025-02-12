fd46e02ad0902592f7fca460d45e0d9dd1158fcb
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-02-12 14:58:49 +0100

    add validator

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:garage_parrot/components/customfield.dart';
+import 'package:garage_parrot/src/utils/validator.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // Déclaration de TextEditingController pour gérer les champs
-  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
 
-  final FocusNode usernameFocus = FocusNode();
+  final FocusNode emailFocus = FocusNode();
+
   final FocusNode passwordFocus = FocusNode();
+
+  final Validator validator = Validator();
+
+  // Clé du formulaire
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // Libération des ressources
+    emailController.dispose();
+    passwordController.dispose();
+    emailFocus.dispose();
+    passwordFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,54 +42,48 @@ class LoginPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CustomField(
-                controller: usernameController,
-                customLabel: "Nom",
-                customHintText: "Veuillez saisir votre nom",
-                customFocus: usernameFocus,
-                customRequestFocus: passwordFocus),
-            CustomField(
-              controller: passwordController,
-              customLabel: "Mot de passe",
-              customHintText: "Veuillez saisir votre mot de passe",
-              customFocus: passwordFocus,
-              customRequestFocus: passwordFocus,
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Vérification des champs
-                if (usernameController.text.isEmpty ||
-                    passwordController.text.isEmpty) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text("Erreur"),
-                        content: const Text("Veuillez remplir tous les champs."),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text("OK"),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                } else {
-                  // Afficher un message d'erreur si les champs sont vides
-                  // Par exemple, naviguer vers la page d'accueil ou vérifier les informations d'identification
-                  Navigator.pushReplacementNamed(context, '/home');
-                }
-              },
-              child: const Text('Se connecter'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              CustomField(
+                controller: emailController,
+                customLabel: "Email",
+                customHintText: "Veuillez saisir votre email",
+                customFocus: emailFocus,
+                customRequestFocus: passwordFocus,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez saisir votre email';
+                  }
+                  return validator.verifyEmail(value);
+                },
+              ),
+              CustomField(
+                controller: passwordController,
+                customLabel: "Mot de passe",
+                customHintText: "Veuillez saisir votre mot de passe",
+                customFocus: passwordFocus,
+                customRequestFocus: passwordFocus,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez saisir le mot de passe';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacementNamed(context, '/home');
+                  }
+                },
+                child: const Text('Se connecter'),
+              ),
+            ],
+          ),
         ),
       ),
     );
