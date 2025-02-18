81867eb3ae16cb1a5fa2207e4710e1087afd81ad
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-02-19 00:21:34 +0100

    add comments

--- lib/components/api_service.dart
+++ lib/components/api_service.dart
@@ -6,15 +6,13 @@ import 'package:http/http.dart' as http;
 class ApiService {
   final String baseUrl = "http://127.0.0.1/garageparrot_api/";
 
+  // Méthode pour générer des URI à partir d'un endpoint
   Uri getUri(String endpoint) {
     return Uri.parse('$baseUrl$endpoint');
   }
 
-  Future<bool> login(
-    //BuildContext context,
-    String email,
-    String userpassword,
-  ) async {
+  // Méthode pour la connexion
+  Future<bool> login(String email, String userpassword) async {
     try {
       // Préparation des données à envoyer
       final Map<String, String> loginData = {
@@ -42,63 +40,59 @@ class ApiService {
     }
   }
 
+  // Méthode pour insérer un nouveau travailleur
   Future<bool> insertWorker(Worker worker) async {
-  try {
-    final res = await http.post(
-      getUri("insert_worker.php"),
-      body: worker.toMap(),
-    );
+    try {
+      final res = await http.post(
+        getUri("insert_worker.php"),
+        body: worker.toMap(),
+      );
 
-    if (res.statusCode == 200) {
-      final response = jsonDecode(res.body);
-      return response['success'] == true;
-    } else {
-      throw Exception("Erreur lors de la connexion au serveur");
-    }
-  } catch (e) {
-    throw Exception("Erreur lors de la requête : $e");
-  }
-}
-
-
-  Future<List<Worker>> getWorkers() async {
-  try {
-    var res = await http.get(getUri("get_workers.php"));
-
-    if (res.statusCode == 200 && res.body.isNotEmpty) {
-      var response = jsonDecode(res.body);
-
-      if (response is List) {
-        return response.map((workerJson) => Worker.fromJson(workerJson)).toList();
+      if (res.statusCode == 200) {
+        final response = jsonDecode(res.body);
+        return response['success'] == true;
       } else {
-        throw Exception("Structure de réponse inattendue");
+        throw Exception("Erreur lors de la connexion au serveur");
       }
-    } else {
-      throw Exception("Erreur lors de la récupération des employés");
+    } catch (e) {
+      throw Exception("Erreur lors de la requête : $e");
     }
-  } catch (e) {
-    throw Exception("Erreur lors de la requête : $e");
   }
-}
 
+  // Méthode pour récupérer la liste des travailleurs
+  Future<List<Worker>> getWorkers() async {
+    try {
+      var res = await http.get(getUri("get_workers.php"));
 
-// delete worker
+      if (res.statusCode == 200 && res.body.isNotEmpty) {
+        var response = jsonDecode(res.body);
+
+        if (response is List) {
+          return response.map((workerJson) => Worker.fromJson(workerJson)).toList();
+        } else {
+          throw Exception("Structure de réponse inattendue");
+        }
+      } else {
+        throw Exception("Erreur lors de la récupération des employés");
+      }
+    } catch (e) {
+      throw Exception("Erreur lors de la requête : $e");
+    }
+  }
+
+  // Méthode pour supprimer un travailleur
   Future<void> deleteWorker(
     BuildContext context,
     int workerId,
     void Function(String message) showSuccessDialog,
     void Function(String message) showErrorDialog,
   ) async {
-    //const String uri = "http://127.0.0.1/garageparrot_api/delete_workers.php";
-
     try {
       var res = await http.delete(
         getUri("delete_workers.php"),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode({"id": workerId}),
       );
-      //debugPrint("res : ${res.body}"); // response body
-      //debugPrint("status code: ${res.statusCode}"); // response status code
 
       if (res.statusCode != 200) {
         showErrorDialog("Erreur lors de la connexion au serveur");
