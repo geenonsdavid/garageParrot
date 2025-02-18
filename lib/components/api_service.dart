3d6713e6deaa65b64c441313220edd00bdc40804
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-02-18 18:53:08 +0100

    parameter insertWorker is  Worker

--- lib/components/api_service.dart
+++ lib/components/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:garage_parrot/components/worker.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -24,7 +25,6 @@ class ApiService {
       // Envoi de la requête HTTP POST
       var res = await http.post(getUri("login.php"), body: loginData);
 
-
       // Vérifier si la réponse est réussie (code 200)
       if (res.statusCode == 200) {
         var response = jsonDecode(res.body);
@@ -42,107 +42,45 @@ class ApiService {
     }
   }
 
-  Future<void> insertWorker(
-    List<TextEditingController> controllers,
-    List<FocusNode> focusNodes,
-    void Function(String message) showSuccessDialog,
-    void Function(String message) showErrorDialog,
-  ) async {
-    if (controllers.any((controller) => controller.text.isEmpty)) {
-      showErrorDialog("Veuillez remplir tous les champs");
-      return;
+  Future<bool> insertWorker(Worker worker) async {
+  try {
+    final res = await http.post(
+      getUri("insert_worker.php"),
+      body: worker.toMap(),
+    );
+
+    if (res.statusCode == 200) {
+      final response = jsonDecode(res.body);
+      return response['success'] == true;
+    } else {
+      throw Exception("Erreur lors de la connexion au serveur");
     }
-
-    final Map<String, String> workerData = {
-      "name": controllers[0].text.trim(),
-      "lastname": controllers[1].text.trim(),
-      "email": controllers[2].text.trim(),
-      "phone": controllers[3].text.trim(),
-      "userpassword": controllers[4].text.trim(),
-    };
-    try {
-      final res =
-          await http.post(getUri("insert_worker.php"), body: workerData);
-      debugPrint("respons: ${res.body}");
-      if (res.statusCode != 200) {
-        showErrorDialog("Erreur lors de la connexion au serveur");
-        return;
-      }
-
-      final response = jsonDecode(res.body); //convert the response to json
-      debugPrint("response en tableau: $response");
-
-      //check if the response is successful
-      if ('${response["success"]}' == "true") {
-        showSuccessDialog("Employé créé avec succès");
-        // Clear all controllers
-        for (var controller in controllers) {
-          controller.clear();
-        }
-      } else {
-        showErrorDialog("Erreur lors de la création de l'employé");
-      }
-    } catch (e) {
-      debugPrint("erreur de requête: $e");
-      showErrorDialog("erreur de requête: $e");
-    }
+  } catch (e) {
+    throw Exception("Erreur lors de la requête : $e");
   }
+}
 
-  Future<List<Map<String, dynamic>>> getWorkers() async {
 
-    try {
-      var res = await http.get(getUri("get_workers.php"));
+  Future<List<Worker>> getWorkers() async {
+  try {
+    var res = await http.get(getUri("get_workers.php"));
 
-      // Vérifier si la réponse est réussie (code 200)
-      if (res.statusCode != 200) {
-        debugPrint("Erreur lors de la connexion au serveur");
-        return []; // Retourner une liste vide en cas d'erreur
-      }
-
-      if (res.body.isEmpty) {
-        debugPrint("Réponse vide du serveur");
-        return []; // Retourner une liste vide si la réponse est vide
-      }
-
-      debugPrint("Response body: ${res.body}");
-
-      // Convertir la réponse en JSON
+    if (res.statusCode == 200 && res.body.isNotEmpty) {
       var response = jsonDecode(res.body);
 
-      // Si la réponse est une liste, traiter les données
       if (response is List) {
-        List<Map<String, dynamic>> workers = [];
-
-        for (var worker in response) {
-          int id = int.tryParse(worker['id'].toString()) ??
-              0; // Assurez-vous que 'id' est un int
-          String name = worker['name'];
-          String lastname = worker['lastname'];
-          String email = worker['email'];
-          String phone = worker['phone'];
-
-          // Ajouter chaque employé à la liste
-          workers.add({
-            'id': id,
-            'name': name,
-            'lastname': lastname,
-            'email': email,
-            'phone': phone,
-          });
-
-          debugPrint('Worker: $id, $name $lastname, $email, $phone');
-        }
-
-        return workers; // Retourner la liste des travailleurs
+        return response.map((workerJson) => Worker.fromJson(workerJson)).toList();
       } else {
-        debugPrint("Structure de réponse inattendue");
-        return []; // Retourner une liste vide si la structure est inattendue
+        throw Exception("Structure de réponse inattendue");
       }
-    } catch (e) {
-      debugPrint("Erreur de requête: $e");
-      return []; // Retourner une liste vide en cas d'erreur
+    } else {
+      throw Exception("Erreur lors de la récupération des employés");
     }
+  } catch (e) {
+    throw Exception("Erreur lors de la requête : $e");
   }
+}
+
 
 // delete worker
   Future<void> deleteWorker(
@@ -178,45 +116,5 @@ class ApiService {
       debugPrint("erreur de requête: $e");
       showErrorDialog("erreur de requête: $e");
     }
-  }
-
-  void showErrorDialog(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Erreur"),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              child: const Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void showSuccessDialog(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Succès"),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              child: const Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 }
