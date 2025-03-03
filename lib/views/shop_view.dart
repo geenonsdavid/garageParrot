508243f959c469fe7d61da165ce3c6953819682c
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-03-03 14:29:57 +0100

    change path

--- lib/views/shop_view.dart
+++ lib/views/shop_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:garage_parrot/components/footer.dart';
 import 'package:garage_parrot/components/header.dart';
-import 'package:garage_parrot/views/list_cars_view.dart';
+import 'package:garage_parrot/models/user_model.dart';
+import 'package:garage_parrot/components/list_cars_view.dart';
+import 'package:provider/provider.dart';
 
 class ShopView extends StatefulWidget {
   const ShopView({super.key, required this.title});
@@ -17,16 +19,20 @@ class _ShopViewState extends State<ShopView> {
   Widget build(BuildContext context) {
     final currentWidth = MediaQuery.of(context).size.width;
     bool isScreenPhone;
-    bool isAdmin = true;
     // Responsivité : détermine si l'écran est un téléphone
     if (currentWidth < 480) {
       isScreenPhone = true;
     } else {
       isScreenPhone = false;
     }
+    bool isAdmin = Provider.of<UserModel>(context, listen: false).role == "admin";
 
     return Scaffold(
-      appBar: Header(title: widget.title, isScreenPhone: isScreenPhone),
+      appBar: Header(
+        title: widget.title,
+        isScreenPhone: isScreenPhone,
+        userRole: isAdmin ? "admin" : "user",
+      ),
       body: ListView(
         children: [
           ListCarsView(isScreenPhone: isScreenPhone, isAdmin: isAdmin),
