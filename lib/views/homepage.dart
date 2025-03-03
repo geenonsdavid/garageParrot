054ae338c63889ffee0f6c4106ebdf2e36c6a920
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-03-03 14:28:38 +0100

    add user role

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:garage_parrot/components/footer.dart';
-import 'package:garage_parrot/views/list_services_view.dart';
+import 'package:garage_parrot/models/user_model.dart';
+import 'package:garage_parrot/components/list_services_view.dart';
+import 'package:provider/provider.dart';
 import '../components/header.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({
-    super.key,
-    required this.title});
+  const HomePage({super.key, required this.title});
 
   final String title;
 
@@ -17,7 +17,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    const isAdmin = false;
+    
+    bool isAdmin = Provider.of<UserModel>(context).role == "admin";
+
     final currentWidth = MediaQuery.of(context).size.width;
     bool isScreenPhone;
 
@@ -28,21 +30,20 @@ class _HomePageState extends State<HomePage> {
       isScreenPhone = false;
     }
 
-    
     return Scaffold(
-      appBar: Header(title: widget.title, isScreenPhone: isScreenPhone),
+      appBar: Header(
+        title: widget.title,
+        isScreenPhone: isScreenPhone,
+        userRole: isAdmin ? "admin" : "user",
+      ),
       body: ListView(
         children: [
           ListServicesView(isScreenPhone: isScreenPhone, isAdmin: isAdmin),
 
           // Widget pour afficher le pied de page
           Footer(isScreenPhone: isScreenPhone),
-          
         ],
       ),
     );
   }
 }
-
-
-
