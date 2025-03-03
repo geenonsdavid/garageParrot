7767f2f838f2ecf3b14a04de59fd618917252f45
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2025-03-03 14:23:33 +0100

    add admin route

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:garage_parrot/models/user_model.dart';
 import 'package:garage_parrot/views/admin_view.dart';
 import 'package:garage_parrot/views/homepage.dart';
 import 'package:garage_parrot/themes/theme.dart';
 import 'package:garage_parrot/views/login_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context)=>UserModel(),
+      child: const MyApp(),
+      )
+    );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   final String title = 'Garage V. Parrot';
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,7 +31,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/home': (context) => HomePage(title: title),
         '/login': (context) => const LoginPage(), 
-        '/admin' : (context) =>  const AdminView(title: "admin"),// Route vers la page de connexion
+        '/admin' : (context) =>  const AdminView(title: "Admin page"),// Route vers la page de connexion
       },
     );
   }
